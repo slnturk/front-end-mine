be90dd01a4a9f28a58b710099d2013b75e9167d3
slnturk <sln.turk98@gmail.com>
2024-11-21 23:51:50 +0300

    deneme 1

--- main.js
+++ main.js
@@ -137,3 +137,122 @@ function circleActivity(circles,carouselIndex) {
         }
     })
 }
+
+/*const fs = require('fs')
+const filePath = 'şirketler'
+fs.readdir(filePath,(err,files)=>{
+    if(err){
+        console.error('klasör okunamadı:',err)
+        return;
+    }
+    console.log('klasör içeriği:');
+    files.forEach(file=>{
+        console.log(file)
+    })
+})
+console.log(filePath)*/
+
+const imagePaths = ['şirketler/acibadem-sigorta.png','şirketler/ak-sigorta.png','şirketler/allianz.png',
+    'şirketler/anadolu-hayat-emeklilik.png','şirketler/anadolu-sigorta.png','şirketler/aveon-global-sigorta.png',
+    'şirketler/axa-hayat-emeklilik.png','şirketler/axa-sigorta.png','şirketler/demir-saglik.png',
+    'şirketler/doga-sigorta.png','şirketler/eureko.png','şirketler/fiba-sigorta.png',
+    'şirketler/gig-sigorta.png','şirketler/hdi-sigorta.png','şirketler/magdeburger-sigorta.png',
+    'şirketler/mapfre-sigorta.png','şirketler/neova-sigorta.png','şirketler/orient-sigorta.png',
+    'şirketler/quick-sigorta.png','şirketler/ray-sigorta.png','şirketler/sompo-sigorta.png',
+    'şirketler/turk-nippon-sigorta.png','şirketler/turkiye-sigorta.png','şirketler/unico.png',
+    'şirketler/zurich-sigorta.png']
+
+    const sirketContainer = document.querySelector('.şirket-container')
+
+imagePaths.forEach((imagePath,index)=>{
+    if(index<10){
+        const card = document.createElement('div')
+        card.className = 'şirket-card'
+    
+        const img = document.createElement('img')
+        img.src = imagePath
+        img.className = 'şirket-img'
+    
+        card.appendChild(img)
+        sirketContainer.appendChild(card)
+    } else {
+        const card = document.createElement('div')
+        card.className = 'şirket-card d-none'
+    
+        const img = document.createElement('img')
+        img.src = imagePath
+        img.className = 'şirket-img'
+    
+        card.appendChild(img)
+        sirketContainer.appendChild(card)
+    }
+    
+})
+
+const width = document.querySelector('.şirket-card').clientWidth
+sirketContainer.style.width = `${(width*5)+(20*4)}px`
+
+let secretCards = []
+
+document.getElementById('see-more').addEventListener('click',()=>{
+    if(secretCards.length === 0){
+        Array.from(document.querySelectorAll('.şirket-card.d-none')).forEach(card => {
+            secretCards.push(card)
+        })
+    }
+    document.querySelector('#see-more i').classList.toggle('bi-chevron-down')
+    document.querySelector('#see-more i').classList.toggle('bi-chevron-up')
+    Array.from(document.querySelectorAll('#see-more span')).forEach(span=>{span.classList.toggle('d-none')})
+    secretCards.forEach(card =>{
+        card.classList.toggle('d-none')
+    })
+})
+
+const tostRow = Array.from(document.querySelectorAll('.tost-row'))
+const tostMenu = Array.from(document.querySelectorAll('.tost-menü'))
+
+function toggleTost(i) {
+    tostMenu.forEach((t,index) => {
+        if(t.classList.contains('open')){
+            t.classList.remove('open')
+            t.classList.add('close')
+            const icon = t.querySelector('i')
+            icon.classList.remove('bi-chevron-up')
+            icon.classList.add('bi-chevron-down')
+        } else if(index===i){
+            t.classList.remove('close')
+            t.classList.add('open')
+            const icon = t.querySelector('i')
+            icon.classList.remove('bi-chevron-down')
+            icon.classList.add('bi-chevron-up')
+        }
+    })
+}
+
+tostRow.forEach((row,index) => {
+    row.addEventListener('click', () => {
+        toggleTost(index)
+    })
+})
+
+/*const imagePath = ['sigortalar/traffic.svg','sigortalar/auto-v2.svg','sigortalar/imm.svg','sigortalar/complementhealth-v2.svg','sigortalar/health-v2.svg','sigortalar/travel.svg',
+    'sigortalar/dask.svg','sigortalar/traffic.svg',
+]
+
+const sigortaContainer = documet.querySelector('.sigorta-container')
+
+/*
+imagePath.forEach((imagePath,index)=>{
+    if(index<10)
+})
+*/
+
+/*const card = document.createElement('div')
+card.className = 'sigorta-card'
+
+const img = document.createElement('img')
+img.src = imagePath
+img.className = 'sigorta-img'
+
+card.appendChild(img)
+sigortaContainer.appendChild(card) */
